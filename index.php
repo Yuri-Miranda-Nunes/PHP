6941e2869441279513454b48c4096747e2e4f08b
Yuri-Miranda-Nunes <yurimirandanunes1@gmail.com>
2025-06-09 10:52:15 -0300

    Add initial PHP script with variable declaration and output examples

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+// comentario
+# comentario tbm
+
+/* 
+    comentario
+    de 
+    muitas linhas
+*/ 
+
+// comentario falando q es uma variavel
+$nome = "André";
+
+// comentario da concatenação
+echo "Olá, " . $nome . "!<br>";
+
+echo "Olá, $nome!<br>"; // saida com echo
+print ("Olá!");
+?>
